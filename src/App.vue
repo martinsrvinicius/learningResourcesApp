50b4bbb91c7c1a4c1d0c46031dfc2428d97a9711
Vinicius <martinsrvinicius@gmail.com>
2022-09-28 09:02:22 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <learning-resource v-for="res in storedResources" :key='res.id'
+    :title="res.title"
+    :description="res.description"
+    :link="res.link"/>
+  </ul>
+</template>
+
+<script>
+import LearningResource from '@/components/resources/LearningResource';
+
+export default {
+  components: {
+    LearningResource
+  },
+  name: 'App',
+  data() {
+    return {
+      storedResources: [
+        { id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official documentation',
+          link: 'https://vuejs.org'
+        },
+        { id: 'google',
+          title: 'Official Google Guide',
+          description: 'The official documentation',
+          link: 'https://google.com'
+        }
+      ]
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/resources/LearningResource.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>
+    linha teste
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href='link'>View Resource</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'LearningResource',
+  props: ['title', 'description', 'link'],
+};
+</script>
+
+<style scoped>
+
+</style>
